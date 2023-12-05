d84354e4d018f43ba7d25449ef2a3950bb46c012
GPla <36087062+GPla@users.noreply.github.com>
2023-12-06 00:16:15 +0100

    added part 2
    reverse search from possible locations to find a valid seed

--- 2023/5/solution.py
+++ 2023/5/solution.py
@@ -1,8 +1,10 @@
 # %%
 from dataclasses import dataclass
 from typing import Optional
-from itertools import batched
 from tqdm import tqdm
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
 
 @dataclass
 class Entry:
@@ -17,6 +19,13 @@ class Entry:
 
         return self.destination + offset
 
+    def reverse_map(self, value: int) -> Optional[int]:
+        offset = value - self.destination
+        if offset < 0 or offset >= self.range:
+            return None
+
+        return self.source + offset
+
 
 @dataclass
 class Map:
@@ -29,48 +38,123 @@ class Map:
                 return output
         return value
 
+    def reverse_convert(self, value: int) -> int:
+        for entry in self.entries:
+            if (output := entry.reverse_map(value)) is not None:
+                return output
+        return value
+
     @classmethod
     def parse(cls, block: str):
         name, *entries = block.splitlines()
-        name = name.replace(' map:', '')
-        return Map(
-            name=name,
-            entries=[Entry(*map(int, entry.split(' '))) for entry in entries],
-        )
+        name = name.replace(" map:", "")
+        return Map(name=name, entries=cls.generate_entries(*entries))
+
+    @classmethod
+    def generate_entries(cls, *lines: str):
+        entries = [Entry(*map(int, line.split(" "))) for line in lines]
+        min_entry = min(entries, key=lambda e: e.destination)
+        if min_entry.destination != 0:
+            entries.append(Entry(0, 0, min_entry.destination))
+        return entries
 
 
-def resolve(seed: int, maps: list[Map], verbose: bool = False):
-    if verbose:
-        print('seed', seed)
+@dataclass
+class RangeSeed:
+    value: int
+    range: int
 
-    value = seed
-    for map in maps:
-        value = map.convert(value)
+    def contains(self, value: int) -> bool:
+        return self.value <= value < self.value + self.range
+
+
+class Solver:
+    @staticmethod
+    def resolve(seed: int, maps: list[Map], verbose: bool = False):
         if verbose:
-            print(map.name.split('-')[-1], value)
+            print("seed", seed)
 
-    return value
+        value = seed
+        for map in maps:
+            value = map.convert(value)
+            if verbose:
+                print(map.name.split("-")[-1], value)
+
+        return value
+
+    @staticmethod
+    def reverse_resolve(location: int, maps: list[Map], verbose: bool = False):
+        if verbose:
+            print("location", location)
+
+        value = location
+        for map in maps[::-1]:
+            value = map.reverse_convert(value)
+            if verbose:
+                print(map.name.split("-")[0], value)
+
+        return value
+
+    @staticmethod
+    def reverse_search(
+        seeds: list[RangeSeed],
+        maps: list[Map],
+        verbose: bool = True,
+    ) -> Optional[int]:
+        sorted_entries = sorted(maps[-1].entries, key=lambda e: e.destination)
+        for entry in tqdm(sorted_entries, "Entries"):
+            if verbose:
+                print(f"Searching {entry.range} from {entry.destination}.")
+
+            possible_locations = range(
+                entry.destination,
+                entry.destination + entry.range,
+            )
+
+            possible_seeds = Parallel()(
+                delayed(Solver._reverse_search_location)(location, maps)
+                for location in tqdm(possible_locations, position=1)
+            )
+
+            location, seed = next(
+                (
+                    (location, seed)
+                    for (location, seed) in possible_seeds
+                    if seed is not None
+                ),
+                (None, None),
+            )
+
+            if seed is not None:
+                return seed, location
+
+    @staticmethod
+    def _reverse_search_location(location: int, maps: list[Map]):
+        possible_seed = Solver.reverse_resolve(location, maps)
+        for seed in seeds:
+            if seed.contains(possible_seed):
+                print(f"Matched seed '{seed}' to location '{location}'.")
+                return location, seed
+        return (location, None)
 
 
-with open('inputs/test.txt') as file:
+with open("inputs/test.txt") as file:
     lines = file.read()
 
-seeds, *blocks = lines.split('\n\n')
-seeds = list(map(int, seeds.split(' ')[1:]))
+seeds, *blocks = lines.split("\n\n")
+seeds = list(map(int, seeds.split(" ")[1:]))
 maps = [Map.parse(block) for block in blocks]
 
-locations = [resolve(seed, maps, verbose=False) for seed in seeds]
+locations = [Solver.resolve(seed, maps, verbose=False) for seed in seeds]
 
 
+# %%
 # Part 1
-print('Part 1')
-print('Answer:', min(locations))
+print("Part 1")
+print("Answer:", min(locations))
 
 # Part 2
-print('Part 2')
-locations = [
-    resolve(seed, maps, verbose=False)
-    for (start, stop) in tqdm(batched(seeds, n=2), 'Seeds')
-    for seed in tqdm(range(start, start + stop), 'Seed', position=2)
-]
-print('Answer:', min(locations))
+print("Part 2")
+seeds = [RangeSeed(s, r) for s, r in list(zip(seeds[0::2], seeds[1::2]))]
+Solver.reverse_search(seeds, maps)
+# Still takes like 15mins to find the correct seed :/
